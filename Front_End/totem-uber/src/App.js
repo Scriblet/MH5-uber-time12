b93be9575547b76edb5a690b34b305d904768402
LughWalle <civalfran1@gmail.com>
2020-11-15 01:40:31 -0300

    teste

--- Front_End/totem-uber/src/App.js
+++ Front_End/totem-uber/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="text-header">
-          <p>{text}</p>
+          <h1 className="text-title">{text}</h1>
         </div>
         <div>
           <img src={girlQr} alt="girl QR code"></img>
