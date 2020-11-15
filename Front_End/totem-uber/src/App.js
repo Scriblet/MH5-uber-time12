84a6dc967b04389535fd82872c814ac46c465c02
LughWalle <48294395+LughWalle@users.noreply.github.com>
2020-11-15 12:11:26 -0300

    Revert "making interface"
    This reverts commit 39de37eff67905b9b34741d564c993d426fc2612.

--- Front_End/totem-uber/src/App.js
+++ Front_End/totem-uber/src/App.js
@@ -1,20 +1,13 @@
-import girlQr from './img/Mulher_Celular.svg';
+import logo from './logo.svg';
 import './App.css';
-const text = `
-  solicite\n
-  seu UBER\n
-  Aqui!!
-`
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="text-header">
-          <p>{text}</p>
-        </div>
-        <div>
-          <img src={girlQr} alt="girl QR code"></img>
-        </div>
+        <p>
+          #VQV_HACKTHON
+        </p>
       </header>
     </div>
   );
