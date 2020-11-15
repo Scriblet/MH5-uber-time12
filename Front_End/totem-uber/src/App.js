39de37eff67905b9b34741d564c993d426fc2612
LughWalle <civalfran1@gmail.com>
2020-11-15 00:57:12 -0300

    making interface

--- Front_End/totem-uber/src/App.js
+++ Front_End/totem-uber/src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
+import girlQr from './img/Mulher_Celular.svg';
 import './App.css';
-
+const text = `
+  solicite\n
+  seu UBER\n
+  Aqui!!
+`
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          #VQV_HACKTHON
-        </p>
+        <div className="text-header">
+          <p>{text}</p>
+        </div>
+        <div>
+          <img src={girlQr} alt="girl QR code"></img>
+        </div>
       </header>
     </div>
   );
