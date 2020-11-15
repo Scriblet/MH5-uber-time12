e173afc6d76ad00a691109bc54c885a32adbbcac
LughWalle <48294395+LughWalle@users.noreply.github.com>
2020-11-15 12:11:26 -0300

    Revert "Botao de Solicite Agora direto na pagina"
    This reverts commit 0bd4ff4da2a84ebed082a862044ab0c487e8b68f.

---
+++ Front_End/totem-uber/src/components/btn_solicite.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class btn_solicite extends Component {
+    render() {
+        return (
+            <div>
+                <button className="btn_solicite">Solicitar agora</button>
+            </div>
+        )
+    }
+}
