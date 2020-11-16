7337ed98fdb5b310cdc55837eceaf892bf285134
Thiago Tavares <thiagooo15@gmail.com>
2020-11-15 23:14:40 -0300

    qr ta biitu

--- Front_End/totem-uber/src/component/qr_code.js
+++ Front_End/totem-uber/src/component/qr_code.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import webCam from './js/cam.js';
+import React from "react";
+import webCam from "./js/cam.js";
+import "./css/qr.css";
 
 class QrCode extends React.Component {
   componentDidMount() {
     webCam();
   }
   render() {
-    return(
+    return (
       <div className="read-qr">
-          <div>
+        <div>
+          <header className="header-qr">
             <h1 className="text-title-qr">Aproxime seu QR Code no leitor</h1>
-          </div>
-          <div>
-            <video autoplay="true" id="webCam">
-            </video>
-            <div className="cancel-qr">
-              <button className="btn-cancel-qr">Cancelar</button>
-            </div>
+          </header>
+        </div>
+        <div>
+          <video autoplay="true" id="webCam"></video>
+          <div className="cancel-qr">
+            <button className="btn-cancel-qr">Cancelar</button>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
+  }
 }
 
 export default QrCode;
