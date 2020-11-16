0427d9b48c89372085f8387dec9bcfc20136e549
LughWalle <civalfran1@gmail.com>
2020-11-15 23:27:16 -0300

    falta a parte do perfil

--- Front_End/totem-uber/src/App.js
+++ Front_End/totem-uber/src/App.js
@@ -2,13 +2,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Initial from './component';
 import QrCode from './component/qr_code';
+import PackPlans from './component/packPlans.js'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-      <Route exact path="/" component={ Initial } />
-      <Route exact path="/qr_code" component={ QrCode } />
+        <Route exact path="/" component={ Initial } />
+        <Route exact path="/qr_code" component={ QrCode } />
+        <Route exact path="/qr_code/result" component={ PackPlans } />
       </Switch>
     </BrowserRouter>
   );
